9a7b662bb851755c5829644bce017b645406c47d
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-11-10 00:59:36 +0900

    Feat: 타이머 초기화 기능

--- main.js
+++ main.js
@@ -1,67 +1,43 @@
-const start = document.querySelector('.start')
+const leftBtn = document.querySelector('.left')
+const rightBtn = document.querySelector('.right')
 const hourEl = document.querySelector('.hour')
 const minEl = document.querySelector('.min')
 const secEl = document.querySelector('.sec')
 
-// 타이머 시작
-start.addEventListener('click',
-(e) => {
-  startTimer(+hourEl.value,+minEl.value,+secEl.value)
-})
-
-function startTimer(hv,mv,sv){
-  console.log('hi')
-  if(hv <0 || mv <0 || sv <0 ){
-    alert('시간을 제대로 입력해주세요.')
+leftBtn.addEventListener('click' , (e) => start(e))
+rightBtn.addEventListener('click' ,(e) => start(e))
+let pause = false
+let timer
+function start(e){
+  let hour,min,sec
+  if(pause === true){ 
+    pause = !pause
+    clearInterval(timer)
+    return
+  }else{
+    pause = !pause
+  }
+  if(e.currentTarget.dataset.name === 'right'){
+    clearInterval(timer)
+    hourEl.value = ''
+    minEl.value = ''
+    secEl.value = ''
     return
   }
-  let hour, min, sec,checkMin;
-  let time = hv*60*60 + mv*60 + sv
-  if(time === 0) return
-  // if(start.lastElementChild.textContent === 'start'){
-
-  // }
-  setInterval(function(){
-    if(time === 0) { // 0이면 정지
-      clearInterval()
+  let time = +hourEl.value*3600 + +minEl.value*60 + +secEl.value
+  timer  = setInterval(() => {
+    if(time === 0) {
+  
+      clearInterval(timer)
       return
     }
-    // console.log(start.firstElementChild)
-    // console.log(start.lastElementChild)
-    time--; // 1초마다 증가, 타이머의 경우 time--;
-
-      hour = Math.floor(time/3600);
-      checkMin = Math.floor(time/60);
-      sec = time%60
-      min = checkMin%60
-
-      let th = hour;
-      let tm = min;
-      let ts = sec;
-      
-      // 한자리일 경우 처리
-      if(th < 10){
-          th = "0" + hour;
-      }
-      if(tm < 10){
-          tm = "0" + min;
-      }
-      if(ts < 10){
-          ts = "0" + sec;
-      }
-
-      // 함수 호출 당시 받은 object의 html 교체
-      hourEl.vlaue = th
-      minEl.value = tm
-      secEl.value = ts
-      
-  }
-, 1000);
-}
-
-const resetBtn = document.querySelector('.reset')
-resetBtn.addEventListener('click',reset)
-
-function reset(){
-  clearInterval(setInterval)
+    time --
+    min = Math.floor(time/60);
+    hour = Math.floor(min/60);
+    sec = time%60;
+    min = min%60;
+    hourEl.value = String(hour).padStart(2,'0')
+    minEl.value = String(min).padStart(2,'0')
+    secEl.value = String(sec).padStart(2,'0')
+  },1000)
 }
