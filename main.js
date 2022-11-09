1e504f8cbd45757702c73e27492325deb4769a1a
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-11-09 23:20:14 +0900

    Feat: 정지 기능

--- main.js
+++ main.js
@@ -2,27 +2,42 @@ const start = document.querySelector('.start')
 const hourEl = document.querySelector('.hour')
 const minEl = document.querySelector('.min')
 const secEl = document.querySelector('.sec')
-start.addEventListener('click',() => startTimer(+hourEl.value,+minEl.value,+secEl.value))
+
+// 타이머 시작
+start.addEventListener('click',
+(e) => {
+  startTimer(+hourEl.value,+minEl.value,+secEl.value)
+})
+
 function startTimer(hv,mv,sv){
+  console.log('hi')
   if(hv <0 || mv <0 || sv <0 ){
     alert('시간을 제대로 입력해주세요.')
     return
   }
-  console.log(hv,mv,sv)
-  var hour, min, sec;
+  let hour, min, sec,checkMin;
   let time = hv*60*60 + mv*60 + sv
-  var timer = setInterval(function(){
-    if(time === 0) return
+  if(time === 0) return
+  // if(start.lastElementChild.textContent === 'start'){
+
+  // }
+  setInterval(function(){
+    if(time === 0) { // 0이면 정지
+      clearInterval()
+      return
+    }
+    // console.log(start.firstElementChild)
+    // console.log(start.lastElementChild)
     time--; // 1초마다 증가, 타이머의 경우 time--;
 
-      min = Math.floor(time/60);
-      hour = Math.floor(min/60);
-      sec = time%60;
-      min = min%60;
+      hour = Math.floor(time/3600);
+      checkMin = Math.floor(time/60);
+      sec = time%60
+      min = checkMin%60
 
-      var th = hour;
-      var tm = min;
-      var ts = sec;
+      let th = hour;
+      let tm = min;
+      let ts = sec;
       
       // 한자리일 경우 처리
       if(th < 10){
@@ -42,4 +57,11 @@ function startTimer(hv,mv,sv){
       
   }
 , 1000);
+}
+
+const resetBtn = document.querySelector('.reset')
+resetBtn.addEventListener('click',reset)
+
+function reset(){
+  clearInterval(setInterval)
 }
