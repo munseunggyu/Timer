017b4a479ced535013e363d1024ffc06e7bcc6ef
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-11-10 16:15:24 +0900

    Fix: 시간을 입력하지 않았을때 오류 해결

--- main.js
+++ main.js
@@ -1,46 +1,56 @@
-const leftBtn = document.querySelector('.left')
-const rightBtn = document.querySelector('.right')
-const hourEl = document.querySelector('.hour')
-const minEl = document.querySelector('.min')
-const secEl = document.querySelector('.sec')
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
+const hourEl = document.querySelector(".hour");
+const minEl = document.querySelector(".min");
+const secEl = document.querySelector(".sec");
 
-leftBtn.addEventListener('click' , (e) => start(e))
-rightBtn.addEventListener('click' ,(e) => start(e))
-let pause = false
-let timer
-function start(e){
-  let hour,min,sec
-  if(e.currentTarget.dataset.name === 'left'){
-    if(pause === true){ 
-      pause = false
-      clearInterval(timer)
-      return
-    }else{
-      pause = true
+leftBtn.addEventListener("click", (e) => start(e));
+rightBtn.addEventListener("click", (e) => start(e));
+let pause = false;
+let timer;
+function start(e) {
+  let hour, min, sec;
+  let time = +hourEl.value * 3600 + +minEl.value * 60 + +secEl.value;
+  if (e.currentTarget.dataset.name === "left") {
+    if (pause === true) {
+      pause = false;
+      clearInterval(timer);
+      return;
+    } else {
+      if (time === 0) {
+        alert("시간을 입력해주세요");
+        return;
+      } else if (time < 0) {
+        alert("시간을 제대로 입력해주세요.");
+        hourEl.value = "";
+        minEl.value = "";
+        secEl.value = "";
+        return;
+      }
+      pause = true;
     }
   }
-  if(e.currentTarget.dataset.name === 'right'){
-    clearInterval(timer)
-    pause = false
-    hourEl.value = ''
-    minEl.value = ''
-    secEl.value = ''
-    return
+  if (e.currentTarget.dataset.name === "right") {
+    clearInterval(timer);
+    pause = false;
+    hourEl.value = "";
+    minEl.value = "";
+    secEl.value = "";
+    return;
   }
-  let time = +hourEl.value*3600 + +minEl.value*60 + +secEl.value
-  timer  = setInterval(() => {
-    if(time === 0) {
-  
-      clearInterval(timer)
-      return
+  timer = setInterval(() => {
+    if (time === 0) {
+      clearInterval(timer);
+      alert("타미어가 끝나 브렀으");
+      return;
     }
-    time --
-    min = Math.floor(time/60);
-    hour = Math.floor(min/60);
-    sec = time%60;
-    min = min%60;
-    hourEl.value = String(hour).padStart(2,'0')
-    minEl.value = String(min).padStart(2,'0')
-    secEl.value = String(sec).padStart(2,'0')
-  },1000)
+    time--;
+    min = Math.floor(time / 60);
+    hour = Math.floor(min / 60);
+    sec = time % 60;
+    min = min % 60;
+    hourEl.value = String(hour).padStart(2, "0");
+    minEl.value = String(min).padStart(2, "0");
+    secEl.value = String(sec).padStart(2, "0");
+  }, 1000);
 }
